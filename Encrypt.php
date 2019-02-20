10da16f38fb6f2274274dce008f68670a69b7220
Marian <memelea@gmail.com>
2019-02-20 23:22:34 +0200

    fix indentation

--- Encrypt.php
+++ Encrypt.php
@@ -4,20 +4,20 @@ class Encrypt
 {
     const KEYS_FOLDER = 'keys';
 
-	private $resource;
+    private $resource;
 
-	public function __construct()
-	{
-  		$this->resource = openssl_pkey_new([
-  			"digest_alg" => "sha512",
-		    "private_key_bits" => 4096,
-		    "private_key_type" => OPENSSL_KEYTYPE_RSA,
-  		]);
-	}
+    public function __construct()
+    {
+        $this->resource = openssl_pkey_new([
+            "digest_alg" => "sha512",
+            "private_key_bits" => 4096,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        ]);
+    }
 
-	public function generateKeys($publicFile = 'public.key', $privateFile = 'private.key')
-	{
-        openssl_pkey_export($this->resource, $privateKey);
+    public function generateKeys($publicFile = 'public.key', $privateFile = 'private.key')
+    {
+	    openssl_pkey_export($this->resource, $privateKey);
 
         $publicKey = openssl_pkey_get_details($this->resource)["key"];
 
@@ -33,9 +33,9 @@ class Encrypt
         file_put_contents(self::KEYS_FOLDER . '/' . $privateFile, $privateKey);
 
         return 'Both keys have been generated and saved in "' . self::KEYS_FOLDER . '" folder.';
-	}
+    }
 
-	public function encrypt($text, $publicFile = 'public.key')
+    public function encrypt($text, $publicFile = 'public.key')
     {
         openssl_public_encrypt($text, $result, file_get_contents(self::KEYS_FOLDER . '/' . $publicFile));
 
